9208f5c1c0991b10284bba020a51b3dace61712d
Daksha Kurhade <dakshakurhade1@gmail.com>
2026-07-25 22:29:08 +0530

    Fix core_overrides.sql: use 'name' argument to match dbt 1.9 ref() signature

--- delivery_dbt/macros/core_overrides.sql
+++ delivery_dbt/macros/core_overrides.sql
@@ -1,9 +1,9 @@
-{% macro ref(model_name, version=None) %}
-  {% set rel = builtins.ref(model_name) %}
+{% macro ref(name, version=None) %}
+  {% set rel = builtins.ref(name) %}
   {% do return(rel.include(database=False)) %}
 {% endmacro %}
 
-{% macro source(source_name, model_name) %}
-  {% set rel = builtins.source(source_name, model_name) %}
+{% macro source(source_name, table_name) %}
+  {% set rel = builtins.source(source_name, table_name) %}
   {% do return(rel.include(database=False)) %}
 {% endmacro %}
